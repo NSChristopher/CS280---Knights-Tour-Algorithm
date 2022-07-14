8eb9c825947640832ca48e611dff1e2ecf660025
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2022-07-14 15:36:43 -0400

    Project submitted

--- main.cc
+++ main.cc
@@ -1,16 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int solveKTUtil(int x, int y, int movei, int visited[8][8],
-                int xMove[], int yMove[]);
 
-int validate(int x, int y, int visited[8][8])
+int validate(int newRow, int newColumn, int visited[8][8])
 {
-    return (!(rowNew < 0 || rowNew > 7 || colNew < 0 || colNew > 7)
-            && visited[x][y] == -1);
+    return (!(newRow < 0 || newRow > 7 || newColumn < 0 || newColumn > 7)
+            && visited[newRow][newColumn] == -1);
 }
 
-int solveKnightTour()
+int knightTour(int visited[8][8], int movesRow[8], int movesColumn[8], int row, int col, int move)
+{
+    int k, newRow, newColumn;
+    if (move == 64) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++)
+                cout << " " << setw(2) << visited[x][y] << " ";
+            cout << endl;
+        }
+        return 1;
+    }
+
+    for (k = 0; k < 8; k++) {
+        newRow = row + movesRow[k];
+        newColumn = col + movesColumn[k];
+        if (validate(newRow, newColumn, visited)) {
+            visited[newRow][newColumn] = move;
+            if (knightTour(visited, movesRow, movesColumn, newRow, newColumn, move + 1)
+                == 1)
+                return 1;
+            else
+                visited[newRow][newColumn] = -1;
+        }
+    }
+    return 0;
+}
+int main()
 {
     int visited[8][8];
     
@@ -18,49 +41,11 @@ int solveKnightTour()
         for (int y = 0; y < 8; y++)
             visited[x][y] = -1;
             
-    int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-    int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-    
+    int movesRow[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int movesColumn[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
     visited[0][0] = 0;
 
-    if (solveKTUtil(0, 0, 1, visited, xMove, yMove) == 0) {
-        cout << "No solution";
-        return 0;
-    }
-    else {
-        for (int x = 0; x < 8; x++) {
-            for (int y = 0; y < 8; y++)
-                cout << " " << setw(2) << visited[x][y] << " ";
-            cout << endl;
-        }
-    }
-    return 1;
-}
-
-int knightTourUtil(int x, int y, int movei, int visited[8][8],
-                int xMove[8], int yMove[8])
-{
-    int k, next_x, next_y;
-    if (movei == 8 * 8)
-        return 1;
-
-    for (k = 0; k < 8; k++) {
-        next_x = x + xMove[k];
-        next_y = y + yMove[k];
-        if (validate(next_x, next_y, visited)) {
-            visited[next_x][next_y] = movei;
-            if (solveKTUtil(next_x, next_y, movei + 1, visited,
-                            xMove, yMove)
-                == 1)
-                return 1;
-            else
-                visited[next_x][next_y] = -1;
-        }
-    }
-    return 0;
-}
-int main()
-{
-    solveKT();
+    knightTour(visited, movesRow, movesColumn ,0, 0, 1);
     return 0;
 }
